d32f74c94dfa40129f5296bb97423133953c764b
v.rosankevich <v.rosankevich@plarium.com>
2021-04-15 20:50:44 +0300

    add coursework project

---
+++ SharedModels/Company.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SharedModels
+{
+    public class Company
+    {
+        public string Name { get; set; }
+        public string Budget { get; set; }
+        public string Name { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
